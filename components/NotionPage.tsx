bea5528e0b2bf42073c31fe2dd06fd3957b0bd85
Muhammad Abdullah Abid <abd@abd.dev>
2024-11-09 21:27:15 -0800

    feat(NotionPage.tsx): comment out GitHubShareButton to temporarily remove it from the UI The GitHubShareButton is commented out to prevent it from rendering, possibly due to a decision to remove or replace it with another feature.
    feat(_app.tsx): integrate Flowise Chatbot for enhanced user interaction
    The Flowise Chatbot is added to the application using the Next.js Script component. This integration provides users with an interactive chatbot experience, enhancing user engagement and support.
    
    chore(site.config.ts): update site configuration to reflect new personal branding
    The site configuration is updated to reflect the new personal branding of Abdullah Abid. This includes changes to the root Notion page ID, site name, domain, author, and social media usernames. Additionally, the navigation style is switched to custom with links to 'About' and 'Contact' pages, aligning with the updated personal branding strategy.

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -28,7 +28,7 @@ import { searchNotion } from '@/lib/search-notion'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
 import { Footer } from './Footer'
-import { GitHubShareButton } from './GitHubShareButton'
+// import { GitHubShareButton } from './GitHubShareButton'
 import { Loading } from './Loading'
 import { NotionPageHeader } from './NotionPageHeader'
 import { Page404 } from './Page404'
@@ -326,7 +326,7 @@ export function NotionPage({
         footer={<Footer />}
       />
 
-      <GitHubShareButton />
+      {/* <GitHubShareButton /> */}
     </>
   )
 }
