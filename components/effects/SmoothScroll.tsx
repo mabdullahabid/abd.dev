25ca9f064cbc0eb93f36709c76b12d169a8b3360
Abdullah Abid <abdullah@autonomoustech.ca>
2026-02-17 19:25:17 +0000

    fix: remove invalid Lenis options (smoothTouch, infinite)

--- components/effects/SmoothScroll.tsx
+++ components/effects/SmoothScroll.tsx
@@ -16,9 +16,7 @@ export default function SmoothScroll() {
         gestureOrientation: 'vertical',
         smoothWheel: true,
         wheelMultiplier: 1,
-        smoothTouch: false,
         touchMultiplier: 2,
-        infinite: false,
       })
 
       function raf(time: number) {
