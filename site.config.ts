9b2f593434d4c8f9956f08c53da0e644354f6b94
Clawdio <abdullah@autonomoustech.ca>
2026-02-17 13:06:21 +0000

    feat: add clean URL for Clawdio @ BNB privacy policy

--- site.config.ts
+++ site.config.ts
@@ -45,7 +45,9 @@ export default siteConfig({
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/privacy/clawdio-bnb': '30af7adc634a81048376fdd6a72ac5a2'
+  },
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
